39fc46e8051617f1f27ffcdf5d07308b5bffa754
shedaniel <daniel@shedaniel.me>
2021-06-10 18:42:38 +0800

    Migrate to crane-plugin

--- bootstrapper/src/main/java/org/spongepowered/asm/mixin/transformer/WhyIsThisPackagePrivate.java
+++
@@ -1,9 +0,0 @@
-package org.spongepowered.asm.mixin.transformer;
-
-public class WhyIsThisPackagePrivate {
-    private final MixinTransformer transformer = new MixinTransformer();
-    
-    public byte[] transformClassBytes(String name, String transformedName, byte[] basicClass) {
-        return transformer.transformClassBytes(name, transformedName, basicClass);
-    }
-}
